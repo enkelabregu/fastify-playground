03ea8b399118d10f9d53d7db85a375a3b71338ba
Enkela Bregu <breguenkela@outlook.com>
2024-05-08 18:47:02 +0200

    setup redis cache

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 import fastify from 'fastify';
+import fastifyRedis from '@fastify/redis';
 
 const app = fastify({
     logger: true,
 })
 
+app.register(fastifyRedis, { url: 'redis-19864.c8.us-east-1-3.ec2.redns.redis-cloud.com:19864', 
+password: 'RLcfp1x1wouNCsYbSf0TT67WfdRWNQRI'  })
+
+// Test Redis connection
+app.ready(err => {
+    if (err) throw err;
+
+    app.redis.ping((err, result) => {
+        if (err) {
+            app.log.error('Redis connection failed', err);
+        } else {
+            app.log.info('Redis connection successful:', result); 
+        }
+    });
+});
 
 app.listen({ port:3001 }, function(err, address){
     if (err) {
